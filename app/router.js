af2a14980823d491db6d630f5ec7ca8cee20ef0e
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-06-04 11:00:07 -0600

    modified router with nested album

--- app/router.js
+++ app/router.js
@@ -6,11 +6,12 @@ var Router = Ember.Router.extend({
 });
 
 
-export default Router.map(function() {
+export default Router.map(function () {
   this.resource('artists', function () {
     this.resource('artist', { path: ':artist_id' }, function () {
-      this.resource('albums');
+      this.resource('albums', function () {
+        this.resource('album', { path: ':album_id' });
+      });
     });
   });
-  this.resource('album', function() {});
 });
